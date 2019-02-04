acb993f31f359413a660baf23c47441f67eb6ebe
Elien Vissers <elien.visserssimilon@gmail.com>
2019-02-04 13:01:54 +0100

    add styling and convert Header component to class component

--- src/header.js
+++ src/header.js
@@ -2,23 +2,36 @@ import React from 'react';
 
 import {ProfilePic} from './profilepic';
 
-export function Header(props) {
-    return (
-        <header>
-            <div className="header-content">
-                <img src="/logo.png" />
-                <h1>My Life Socks!</h1>
-                <ProfilePic
-                    first={props.first}
-                    last={props.last}
-                    pictureUrl={props.pictureUrl}
-                    openUploader={props.openUploader}
-                />
-            </div>
-            <div className="line">
-            </div>
-        </header>
-    );
+export class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <header>
+                <div className="header-content">
+                    <div className="logoname">
+                        <img src="/logo.png" />
+                        <h1>My Life Socks!</h1>
+                    </div>
+                    <div className="nav">
+                        <ProfilePic
+                            first={this.props.first}
+                            last={this.props.last}
+                            pictureUrl={this.props.pictureUrl}
+                            openUploader={this.props.openUploader}
+                        />
+                        <p >Profile</p>
+                        <p>Logout</p>
+                    </div>
+                </div>
+                <div className="line">
+                </div>
+            </header>
+        );
+    }
+
 }
 
 //make the logo a Link to the homepage == users profile --> <Link to="/"><img .../></Link> and wrap everything in a BrowserRouter tag
